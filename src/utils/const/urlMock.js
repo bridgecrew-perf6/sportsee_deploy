3bb3c03d558384b73adc4307abd2387189e439d2
Marion Pluyaud <marion070189@hotmail.fr>
2022-06-29 12:12:22 +0200

    update path

--- src/utils/const/urlMock.js
+++ src/utils/const/urlMock.js
@@ -1,15 +1,15 @@
 export const urlMock = {
     userMainData(id) {
-        return `../data/${id}_main_data.json`
+        return `../../data/${id}_main_data.json`
     },
     userActivity(id) {
-        return `../data/${id}/user_activity.json`
+        return `../../data/${id}/user_activity.json`
     },
     userAverageSessions(id) {
-        return `../data/${id}/user_average_sessions.json`
+        return `../../data/${id}/user_average_sessions.json`
     },
     userPerformances(id) {
-        return `../data/${id}/user_performance.json`
+        return `../../data/${id}/user_performance.json`
     }
 
 }
